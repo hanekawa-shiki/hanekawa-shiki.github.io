e693672711a5809f275618cd8141594a5e18df28
kamishima-kaede <kamiacgxu@gmail.com>
2024-07-08 19:16:28 +0800

    update sidebar

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,8 +3,24 @@ import { sidebar } from 'vuepress-theme-hope';
 export default sidebar({
   '/document/node-doc/': 'structure',
   '/ECMAScript/': 'structure',
-  '/framework_front/vue3/': 'structure',
-  '/framework_front/react/': 'structure',
+  '/framework_front/vue3/': [
+    '1.vue3',
+    '2.create',
+    '3.compositionApi',
+    '4.otherApi',
+    '5.compare',
+    '6.newComponents',
+    '7.others',
+    '8.skill'
+  ],
+  '/framework_front/react/': [
+    '1.react',
+    '2.basic_use',
+    '3.router',
+    '4.redux',
+    '5.other_options',
+    '6.ui_library',
+  ],
   '/website_build/': [
     '0.environment_windows',
     '1.environment_server',
--- docs/framework_front/react/README.md
+++ docs/framework_front/react/README.md
@@ -12,15 +12,3 @@ date: 2021-09-23
 `React` 快速入门教程
 
 <!-- more -->
-
-- [零、React](react.md)
-
-- [一、基本使用](basic_use.md)
-
-- [二、Router](router.md)
-
-- [三、第三方UI库](ui_library.md)
-
-- [四、Redux](redux.md)
-
-- [五、其他注意事项](other_options.md)
